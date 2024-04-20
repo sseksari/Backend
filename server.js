bd0103b298e856335052168f03dd9b78ca67ef18
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-04-19 22:33:46 -0700

    project folder updated

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express=require('express')
 const app=express()
-const geminiAPI=require('./gemini/calls.js');
+const geminiAPI=require('./routers/gemini.js');
 const dotenv=require('dotenv')
 dotenv.config()
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-
+const PORT= 3000 || process.env.PORT
 app.get('/',(req,res)=>{
     res.send("Home page is ready")
 })
 
+
 app.use('/api/gemini/',geminiAPI);
-app.listen('3000',()=>{
+app.listen(PORT,()=>{
     console.log("Server is listening at 3000")
 })
